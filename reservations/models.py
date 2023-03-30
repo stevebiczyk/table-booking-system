8b6e74bba2b4f9ce11405ac68ef03a821021516c
stevebiczyk <stevebiczyk@gmail.com>
2023-03-30 06:34:48 +0000

    Added reservation model

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Reservation(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    notes = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.name} - {self.date} {self.time}"
