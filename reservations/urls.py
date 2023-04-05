97356329c786e5a898be37a5b1268cd624e9a695
stevebiczyk <stevebiczyk@gmail.com>
2023-04-05 01:18:11 +0000

    Completed base.html by adding bootstrap and Google Fonts. Completed the page structure using base.html as the template. Added Bootstrap and CSS styling to navbar

--- reservations/urls.py
+++ reservations/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='index'),
     path('about/', views.about, name='about'),
     path('menu/', views.menu, name='menu'),
     path('gallery/', views.gallery, name='gallery'),
