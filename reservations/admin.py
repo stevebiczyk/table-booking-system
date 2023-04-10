5577dc741f56019c4343a02b48c25b30c7e7da3c
stevebiczyk <stevebiczyk@gmail.com>
2023-04-10 17:17:25 +0000

    Added image gallery structure

--- reservations/admin.py
+++ reservations/admin.py
@@ -5,7 +5,7 @@ from .models import Customer, Reservation, Staff, Table
 class ReservationAdmin(admin.ModelAdmin):
     list_display = ('customer', 'table', 'date', 'time')
     list_filter = ('date', 'time', 'customer', 'table')
-    search_fields = ['customer__first_name', 'customer__last_name']
+    search_fields = ['customer__first_name', 'customer__last_name', 'email']
 
 
 admin.site.register((Customer, Staff, Table))
