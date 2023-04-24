576f3495851a667572215786f6d6814dd80cad68
stevebiczyk <stevebiczyk@gmail.com>
2023-04-24 10:39:35 +0000

    Removed references to customer form from booking.html

--- reservations/forms.py
+++ reservations/forms.py
@@ -17,6 +17,7 @@ class ReservationForm(forms.ModelForm):
             attrs={'type': 'time', 'step': '1800'}, format='%H:%M')
     )
     table = forms.ModelChoiceField(queryset=Table.objects.all(), required=True)
+    # Limiting number of guests to 25 people
     number_of_guests = forms.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(25)]
     )
@@ -34,26 +35,41 @@ class ReservationForm(forms.ModelForm):
     def clean(self):
         cleaned_data = super().clean()
         reservation_time = cleaned_data.get("time")
-        opening_time = datetime.time(9, 0)
-        closing_time = datetime.time(21, 0)
+        opening_time = datetime.time(hour=9, minute=0)
+        closing_time = datetime.time(hour=21, minute=0)
+        # opening_time = datetime.time(9, 0)
+        # closing_time = datetime.time(21, 0)
 
+        # Check if the reservation time is within opening hours
         if reservation_time is not None and (reservation_time < opening_time or reservation_time >= closing_time):
             raise ValidationError(
                 "Please choose a reservation time between 09:00 and 21:00."
             )
 
-    def update_time_choices(self, table, date):
-        reserved_times = Reservation.objects.filter(
-            table=table, date=date).values_list('time', flat=True)
-        available_times = []
+            # Check if the selected date and time are in the past
+        date = cleaned_data.get('date')
+        time = cleaned_data.get('time')
+        if date and time:
+            now = datetime.datetime.now()
+            reservation_datetime = datetime.datetime.combine(date, time)
+            if reservation_datetime < now:
+                raise ValidationError(
+                    "You cannot make a booking for a past date and time.")
+        return cleaned_data
 
-        for hour in range(9, 21):
-            current_time = datetime.time(hour, 0)
-            if current_time not in reserved_times:
-                available_times.append((current_time.strftime(
-                    "%H:%M"), current_time.strftime("%H:%M")))
 
-        self.fields['time'].choices = available_times
+def update_time_choices(self, table, date):
+    reserved_times = Reservation.objects.filter(
+        table=table, date=date).values_list('time', flat=True)
+    available_times = []
+
+    for hour in range(9, 21):
+        current_time = datetime.time(hour, 0)
+        if current_time not in reserved_times:
+            available_times.append((current_time.strftime(
+                "%H:%M"), current_time.strftime("%H:%M")))
+
+    self.fields['time'].choices = available_times
 
 
 class StaffForm(forms.ModelForm):
