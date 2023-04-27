30a7bc94dbdbf3a1a4bdd037a65bb641ccc22656
stevebiczyk <stevebiczyk@gmail.com>
2023-04-27 19:30:26 +0000

    Added unit tests to tests.py. Changed Bootstrap code for margins

--- tablebooking/settings.py
+++ tablebooking/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import sys
 from pathlib import Path
 import os
 import dj_database_url
@@ -20,6 +21,7 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
